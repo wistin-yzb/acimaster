b4b73ed6db9decfa1d94317e3a7e4d0c0c9f517c
git <1391930327@qq.com>
2018-12-21 13:04:00 +0800

    fix tiny update commit

---
+++ application/logs/log-2018-12-18.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-12-18 09:22:09 --> Session: session.auto_start is enabled in php.ini. Aborting.
+ERROR - 2018-12-18 09:22:09 --> 404 Page Not Found: Faviconico/index
---
+++ application/logs/log-2018-12-20.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-12-20 09:28:45 --> Severity: Warning --> mysqli::real_connect():  F:\xampp-win32-5.6.3-0-VC11-installerroot\xammp\htdocs\www\www.acimaster.devp\system\database\drivers\mysqli\mysqli_driver.php 161
+ERROR - 2018-12-20 09:28:45 --> Unable to connect to the database
+ERROR - 2018-12-20 11:39:54 --> Session: session.auto_start is enabled in php.ini. Aborting.
+ERROR - 2018-12-20 11:39:54 --> 404 Page Not Found: Faviconico/index
